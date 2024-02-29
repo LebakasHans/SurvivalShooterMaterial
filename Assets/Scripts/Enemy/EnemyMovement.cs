8979159362f34c21a4f86b5936ba8d9fc9167206
Jakob Wintersteiger <jakob.wintersteiger@gmail.com>
2024-02-29 12:09:35 +0100

    29.02.2024

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,35 +1,34 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
-{ 
+{
     Transform player;
-//    PlayerHealth playerHealth;      // Reference to the player's health.
-//    EnemyHealth enemyHealth;        // Reference to this enemy's health.
+    //    PlayerHealth playerHealth;      // Reference to the player's health.
+    //    EnemyHealth enemyHealth;        // Reference to this enemy's health.
     NavMeshAgent nav;
 
-    void Awake ()
+    void Awake()
     {
-        player = GameObject.FindGameObjectWithTag ("Player").transform;
-//        playerHealth = player.GetComponent<PlayerHealth>();
-//        enemyHealth = GetComponent<EnemyHealth>();
-        nav = GetComponent<NavMeshAgent> ();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //        playerHealth = player.GetComponent<PlayerHealth>();
+        //        enemyHealth = GetComponent<EnemyHealth>();
+        nav = GetComponent<NavMeshAgent>();
     }
 
-    void Update ()
+    void Update()
     {
         // If the enemy and the player have health left...
-//        if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
-//        {
-            // ... set the destination of the nav mesh agent to the player.
-            nav.SetDestination(player.position);
-//        }
+        //        if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        //        {
+        // ... set the destination of the nav mesh agent to the player.
+        nav.SetDestination(player.position);
+        //        }
         // Otherwise...
-//        else
-//        {
-            // ... disable the nav mesh agent.
-//            nav.enabled = false;
-//        }
+        //        else
+        //        {
+        // ... disable the nav mesh agent.
+        //            nav.enabled = false;
+        //        }
     }
 }
